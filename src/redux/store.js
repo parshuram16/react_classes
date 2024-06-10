471b47f5d97d49ac157a03fed045f2d9155a7093
parshuram16 <parshuramnerella16@gmail.com>
2024-06-10 12:05:46 +0530

    dev tools configured

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,14 @@
 
 import { legacy_createStore } from "redux";
 import { singleReducer } from "./profile/singleReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 
-export const reduxStore=legacy_createStore(singleReducer)
+export const reduxStore=legacy_createStore(singleReducer,composeWithDevTools())
+
+
+
+
 
 
 
